f08477c0a8bc142b489d25eb1a4d9333d159c1b9
Gonzer <115153839+fazalgonzer@users.noreply.github.com>
2025-10-01 01:43:32 +0500

    React app testing part1

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+// import Header from "./components/Header";
+// import AddTask from "./components/AddTask";
+// import TaskList from "./components/TaskList";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello, React!</h1>
+      {/* <Header />
+      <AddTask />
+      <TaskList /> */}
+    </div>
+  );
+}
+
+export default App;
